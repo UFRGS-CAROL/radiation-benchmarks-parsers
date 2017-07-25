fb767d1911f38a6de85c8124f5331dc92cb0f7e0
Fernando <f_157@hotmail.com>
2017-07-25 17:55:28 -0300

    darknet v2

--- ParsersClasses/DarknetV2Parser.py
+++ ParsersClasses/DarknetV2Parser.py
@@ -33,7 +33,6 @@ class DarknetV2Parser(ObjectDetectionParser):
     _sizeOfDNN = 0
     _allProperties = None
 
-
     _csvHeader = ["logFileName", "Machine", "Benchmark", "SDC_Iteration", "#Accumulated_Errors", "#Iteration_Errors",
                   "gold_lines", "detected_lines", "wrong_elements", "precision",
                   "recall", "false_negative", "false_positive", "true_positive", "abft_type", "row_detected_errors",
@@ -60,10 +59,6 @@ class DarknetV2Parser(ObjectDetectionParser):
         except:
             print "\n Crash on create parse layers parameters"
             sys.exit(-1)
-
-
-
-
 
     def _writeToCSV(self, csvFileName):
         self._writeCSVHeader(csvFileName)
@@ -93,7 +88,7 @@ class DarknetV2Parser(ObjectDetectionParser):
                           self._header]
 
             if (self._parseLayers):
-                raise  NotImplementedError
+                raise NotImplementedError
 
             writer.writerow(outputList)
             csvWFP.close()
@@ -103,16 +98,81 @@ class DarknetV2Parser(ObjectDetectionParser):
             # raise
 
     def setSize(self, header):
-        print "passou"
+
         self._size = str(self._goldFileName)
 
     def _relativeErrorParser(self, errList):
         raise NotImplementedError
 
-
     # parse Darknet
     # returns a dictionary
     def parseErrMethod(self, errString):
-        print "passou"
+        ret = {}
+        darknetM = re.match(
+            ".*img\: \[(\d+)\].*prob\[(\d+)\]\[(\d+)\].*r\:(\S+).*e\:(\S+).*x_r\: (\S+).*x_e\: "
+            "(\S+).*y_r\: (\S+).*y_e\: (\S+).*w_r\: (\S+).*w_e\: (\S+).*h_r\: (\S+).*h_e\: (\S+).*",
+            errString)
 
-        raise NotImplementedError
+        if darknetM:
+            ret["img"] = int(darknetM.group(1))
+            # ERR img: [0] prob[60][0] r:0.0000000000000000e+00 e:0.0000000000000000e+00
+            ret["prob_i"] = int(darknetM.group(2))
+            ret["prob_j"] = int(darknetM.group(3))
+
+            try:
+                ret["prob_r"] = float(darknetM.group(4))
+            except:
+                ret["prob_r"] = 1e10
+
+            try:
+                ret["prob_e"] = float(darknetM.group(5))
+            except:
+                ret["prob_e"] = 1e10
+
+            # x_r: 1.7303885519504547e-01 x_e: 3.3303898572921753e-01
+            try:
+                ret["x_r"] = float(darknetM.group(6))
+            except:
+                ret["x_r"] = 1e10
+
+            try:
+                ret["x_e"] = float(darknetM.group(7))
+            except:
+                ret["x_e"] = 1e10
+
+            # y_r: 1.0350487381219864e-01 y_e: 1.0350500047206879e-01
+            try:
+                ret["y_r"] = float(darknetM.group(8))
+            except:
+                ret["y_r"] = 1e10
+
+            try:
+                ret["y_e"] = float(darknetM.group(9))
+            except:
+                ret["y_e"] = 1e10
+
+            # w_r: 2.0466668531298637e-02 w_e: 2.0467000082135201e-02
+            try:
+                ret["w_r"] = float(darknetM.group(10))
+            except:
+                ret["w_r"] = 1e10
+
+            try:
+                ret["w_e"] = float(darknetM.group(11))
+            except:
+                ret["w_e"] = 1e10
+
+            # h_r: 4.1410662233829498e-02 h_e: 4.1411001235246658e-02
+            try:
+                ret["h_r"] = float(darknetM.group(12))
+            except:
+                ret["h_r"] = 1e10
+
+            try:
+                ret["h_e"] = float(darknetM.group(13))
+            except:
+                ret["h_e"] = 1e10
+
+            return ret
+
+        return None
