3afa2fcd3db28bbd0319abb22e75cfa9f0b77292
reliabilitylogdata <reliabilitylogdata@github.com>
2017-07-25 23:30:04 -0300

    start cnn parser layer

---
+++ SupportClasses/CNNLayerParser.py
@@ -0,0 +1,38 @@
+
+
+"""
+This class will be used
+for parsing layers in CNN logs
+"""
+class CNNLayerParser():
+    __layerDimentions = {}
+    __layersGoldPath = ""
+    __layersPath = ""
+
+    """
+    Constructor
+    layersDimention : A dict that will have for each layer its 3D dimentions, like this
+    0: [224, 224, 64],  1: [112, 112, 64],   2: [112, 112, 192], ....
+    layerGoldPath : Path for gold layers
+    layerPath :  Path to detection layers
+    externalMethodGenerateGoldLayerName : Generate gold layer name from outside
+    externalMethodGenerateLayerName     : Generate layer name from outside
+    """
+    def __init__(self, **kwargs):
+        self.__layerDimentions = kwargs.pop("layersDimention")
+        self.__layersGoldPath = kwargs.pop("layerGoldPath")
+        self.__layersPath = kwargs.pop("layerPath")
+
+
+
+
+    """
+    This method receives an external method and its parameters to open
+    the layer files
+    """
+    def _openLayers(self, externalMethod, parameter1):
+        raise NotImplemented
+
+
+    def parserLayer(self):
+        raise NotImplemented
