aa59f5c93a9f6975c4266f0f040084f796e22f17
reliabilitylogdata <reliabilitylogdata@github.com>
2017-07-27 11:48:02 -0300

    cnn layer parser

--- SupportClasses/CNNLayerParser.py
+++ SupportClasses/CNNLayerParser.py
@@ -19,7 +19,9 @@ class CNNLayerParser():
     _sizeOfDNN = 0
     _allProperties = None
 
-
+    __errorTypes = ['allLayers', 'filtered2', 'filtered5', 'filtered50']
+    __infoNames = ['smallestError', 'biggestError', 'numErrors', 'errorsAverage', 'errorsStdDeviation']
+    __filterNames = ['allErrors', 'newErrors', 'propagatedErrors']
 
 
     """
@@ -31,13 +33,53 @@ class CNNLayerParser():
     externalMethodGenerateGoldLayerName : Generate gold layer name from outside
     externalMethodGenerateLayerName     : Generate layer name from outside
     """
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         self.__layerDimentions = kwargs.pop("layersDimention")
         self.__layersGoldPath = kwargs.pop("layerGoldPath")
         self.__layersPath = kwargs.pop("layerPath")
 
 
+    def genCsvHeader(self, layersGoldPath, layersPath):
+        self.__layersGoldPath = str(layersGoldPath)
+        self.__layersPath = str(layersPath)
 
+        csvHeader = []
+        csvHeader.extend(self._getLayerHeaderName(layerNum, infoName, filterName)
+                               for filterName in self.__filterNames
+                               for infoName in self.__infoNames
+                               for layerNum in xrange(32))
+        csvHeader.extend(self._getLayerHeaderNameErrorType(layerNum)
+                               for layerNum in xrange(32))
+        csvHeader.extend(self._getMaskableHeaderName(layerNum)
+                               for layerNum in xrange(32))
+        csvHeader.extend(self._getNumCorrectableErrorsHeaderName(layerNum)
+                               for layerNum in [0, 2, 7, 18])
+
+        return csvHeader
+
+    # layer<layerNum>MaskableErrorsNum
+    def _getMaskableHeaderName(self, layerNum):
+        maskableHeaderName = 'layer' + str(layerNum) + 'MaskableErrorsNum'
+        return maskableHeaderName
+
+    # layer<layerNum>ErrorType
+    def _getLayerHeaderNameErrorType(self, layerNum):
+        layerHeaderName = 'layer' + str(layerNum) + 'ErrorType'
+        return layerHeaderName
+
+    """
+    layerHeaderName :: layer<layerNum><infoName>_<filterName>
+    <infoName> :: 'smallestError', 'biggestError', 'numErrors', 'errorsAverage', 'errorsVariance'
+    <filterName> :: 'allErrors', 'newErrors', 'propagatedErrors'
+    """
+    def _getLayerHeaderName(self, layerNum, infoName, filterName):
+        layerHeaderName = 'layer' + str(layerNum) + infoName + '_' + filterName
+        return layerHeaderName
+
+    # layer<layerNum>CorrectableErrorsNum
+    def _getNumCorrectableErrorsHeaderName(self, layerNum):
+        correctableHeaderName = 'layer' + str(layerNum) + 'CorrectableErrorsNum'
+        return correctableHeaderName
 
     """
     This method receives an external method and its parameters to open
