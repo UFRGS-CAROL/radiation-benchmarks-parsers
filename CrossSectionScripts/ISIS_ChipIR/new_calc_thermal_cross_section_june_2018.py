215f212abcfb3fa1644353336c05252e7377864f
gpdavila <gpiscoya2013@hotmail.com>
2018-08-20 20:42:10 -0300

    Rotax Calc Cross Section

--- CrossSectionScripts/ISIS_ChipIR/new_calc_thermal_cross_section_june_2018.py
+++ CrossSectionScripts/ISIS_ChipIR/new_calc_thermal_cross_section_june_2018.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python -u
 
-#----------------------------- 07/06/2018 -----------------------------------
-# 1- The log given by CHIPIR is not a csv, replace spaces and tabs with ; 
-# 2- Not able to check correctness due to absense of Distance Factor 
-#       and Phisical Magic factor
-#----------------------------------------------------------------------------
 
 import os
 import sys
@@ -12,10 +7,7 @@ import re
 import csv
 from datetime import timedelta
 from datetime import datetime
-
-MAGIC_THERMAL_VALUE = 0.016e6
-SMALLEST_RUN = timedelta(minutes=20)
-ONE_HOUR = 60
+import time
 
 
 def get_dt(year_date, day_time, sec_frac):
@@ -58,11 +50,12 @@ def read_count_file(in_file_name):
     return file_lines
 
 
-def get_fluency_flux(start_dt, end_dt, file_lines, factor):
-    # file_lines eh a corrtete
+def get_fluence_flux(start_dt, end_dt, file_lines, factor, distance_factor=1.0): # file_lines eh a corrtete
+
     last_curr_integral = None
     last_dt = None
-
+    new_curr = 0
+    samples = 0
     beam_off_time = 0
     first_curr_integral = None
     # file_lines teve que ser colocado como csv, substituit " " por ; e "tab" por ;
@@ -77,7 +70,7 @@ def get_fluency_flux(start_dt, end_dt, file_lines, factor):
         buff = line[1].split('.')
         day_time = buff[0]
         sec_frac = "."+buff[1]  # Sec Frac nao esta separado no log
-
+        actual_curr = float(line[2])
         curr_integral = float(line[3])
         #print(str(year_date) + str(day_time) + str(sec_frac) + str(curr_integral))
         # Generate datetime for line
@@ -85,23 +78,32 @@ def get_fluency_flux(start_dt, end_dt, file_lines, factor):
 
         if start_dt <= cur_dt and first_curr_integral is None:
             first_curr_integral = curr_integral
+            new_curr = float(line[2])   # Obtemos a corrente que devemos somar.
+            samples = 1                 # Necesario para calcular a media. 
             last_dt = cur_dt
             continue
             
         if first_curr_integral is not None:
-            if curr_integral == last_curr_integral:
+            if actual_curr == 0: #beam off se nao tem corrente
                 beam_off_time += (cur_dt - last_dt).total_seconds() # Adiciona a diferenca do tempo de i e i-1 -> Beam Parado 3S
 
             #print(curr_integral)
             last_curr_integral = curr_integral
             last_dt = cur_dt
+            new_curr += float(line[2])
+            samples += 1
 
         if cur_dt > end_dt:
             #print(cur_dt)
-            interval_total_seconds = float((end_dt - start_dt).total_seconds())
-            flux1h = ((last_curr_integral - first_curr_integral) * factor) / interval_total_seconds
-            # flux1h *= distance_factor
-            return flux1h, beam_off_time
+            #interval_total_seconds = float((end_dt - start_dt).total_seconds())
+            #flux1h = ((last_curr_integral - first_curr_integral) * factor) / interval_total_seconds
+            #flux1h *= distance_factor 
+            
+            thermal_flux = new_curr / samples   # Fluxo eh a corrente media 
+            thermal_flux *= factor              # Multiplicada x 16000 (Jun 2018)
+            thermal_flux *= distance_factor     # Distancia eh a mesma para todos, assim assume-se 1
+            
+            return thermal_flux, beam_off_time
             
         elif first_curr_integral is not None:
             last_curr_integral = curr_integral
@@ -111,150 +113,7 @@ def get_fluency_flux(start_dt, end_dt, file_lines, factor):
     flux1h = -1
     beam_off_time = -1
     return flux1h, beam_off_time
-
-
-def pre_process_data(full_file_lines):
-    """
-    I group all the possible configurations in a dictionary
-    the key is
-    MACHINE + BENCHMARK + LOG HEADER
-    :param full_file_lines:
-    :return: dictionary that contains the grouped benchmarks
-    """
-    grouped_benchmarks = {}
-    for i in full_file_lines:
-        # MACHINE + BENCHMARK + LOG HEADER
-        key = i[1] + i[2] + i[3]
-        if key not in grouped_benchmarks:
-            grouped_benchmarks[key] = []
-
-        grouped_benchmarks[key].append(i)
-
-    return grouped_benchmarks
-
-
-def main():
-    if len(sys.argv) < 2:
-        print "Usage: %s <neutron counts input file> <csv file> "  # <factor> <distance factor file>" % (sys.argv[0])
-        sys.exit(1)
-
-    in_file_name = sys.argv[1]
-    csv_file_name = sys.argv[2]
-    factor = MAGIC_THERMAL_VALUE # float(sys.argv[3])
-    # distance_factor_file = sys.argv[4]
-    # Load all distances before continue
-    # distance_data = get_distance_data(distance_factor_file=distance_factor_file)
-
-    csv_out_file_summary = csv_file_name.replace(".csv", "_cross_section_summary.csv")
-    print "in: " + csv_file_name
-    print "out: " + csv_out_file_summary
-
-    with open(csv_file_name, "r") as csv_input, open(csv_out_file_summary, "w") as csv_summary:
-        # reading and starting the csvs files
-        reader = csv.reader(csv_input, delimiter=';')
-        writer_csv_summary = csv.writer(csv_summary, delimiter=';')
-        header_summary = ["start timestamp", "end timestamp", "Machine", "benchmark",
-                          "header info", "#lines computed", "#SDC", "#AccTime", "#(Abort==0)",
-                          "Flux 1h", "Flux AccTime", "Fluence(Flux * $AccTime)",
-                          "Fluence AccTime(FluxAccTime * $AccTime)", "Cross Section SDC",
-                          "Cross Section Crash", "Time Beam Off (sec)", "Cross Section SDC AccTime",
-                          "Cross Section Crash AccTime", "Time Beam Off AccTime (sec)", "distance_factor"]
-
-        writer_csv_summary.writerow(header_summary)
-        full_lines = list(reader)
-
-        # grouping the benchmarks
-        grouped_benchmarks = pre_process_data(full_file_lines=full_lines)
-
-        # We need to read the neutron count files before calling get_fluency_flux
-        file_lines = read_count_file(in_file_name)
-
-        for bench in grouped_benchmarks:
-            print "Parsing for {}".format(bench)
-            lines = grouped_benchmarks[bench]
-            i = 0
-            while i < len(lines) - 1:
-                try:
-                    start_dt = datetime.strptime(lines[i][0][0:-1], "%c")
-                    end_dt = datetime.strptime(lines[i + 1][0][0:-1], "%c")
-                except ValueError as err:
-                    print err
-                    continue
-
-                machine = lines[i][1]
-                bench = lines[i][2]
-                header_info = lines[i][3]
-                # acc_time
-                acc_time_s = float(lines[i][6])
-                # #SDC
-                sdc_s = int(lines[i][4])
-                # abort
-                abort_zero_s = 0
-                if int(lines[i][7]) == 0 and int(lines[i][8]) == 0:
-                    abort_zero_s += 1
-
-                first_i = i
-                while (end_dt - start_dt) < timedelta(minutes=ONE_HOUR):
-                    i += 1
-                    if i >= (len(lines) - 1):  # end of lines
-                        break
-                    if lines[first_i][2] != lines[i][2] or lines[first_i][3] != lines[i][3]:
-                        print("ISSO ACONTECEU NATURALMENTE")
-                        continue
-
-                    acc_time_s += float(lines[i][6])
-                    sdc_s += int(lines[i][4])
-                    if int(lines[i][7]) == 0 and int(lines[i][8]) == 0:
-                        abort_zero_s += 1
-
-                    end_dt = datetime.strptime(lines[i + 1][0][0:-1], "%c")
-
-                # why consider such a small run???
-                if (end_dt - start_dt) < SMALLEST_RUN:
-                    i += 1
-                    continue
-
-                print "Generating cross section for {}, start {} end {}".format(bench.strip(), start_dt, end_dt)
-
-                # compute 1h flux; sum SDC, ACC_TIME, Abort with 0; compute fluency (flux*(sum ACC_TIME))
-                # distance_factor = check_distance_factor(distance_data=distance_data, start_dt=start_dt,
-                #                                         board=lines[i][1])
-
-                flux, time_beam_off = get_fluency_flux(start_dt=start_dt, end_dt=(start_dt + timedelta(minutes=60)),
-                                                       file_lines=file_lines, factor=factor)
-                flux_acc_time, time_beam_off_acc_time = get_fluency_flux(start_dt=start_dt,
-                                                                         end_dt=(
-                                                                             start_dt + timedelta(seconds=acc_time_s)),
-                                                                         file_lines=file_lines, factor=factor)
-
-                fluency = flux * acc_time_s
-                fluency_acc_time = flux_acc_time * acc_time_s
-                if fluency > 0:
-                    cross_section = sdc_s / fluency
-                    cross_section_crash = abort_zero_s / fluency
-                else:
-                    cross_section = 0
-                    cross_section_crash = 0
-                if fluency_acc_time > 0:
-                    cross_section_acc_time = sdc_s / fluency_acc_time
-                    cross_section_crash_acc_time = abort_zero_s / fluency_acc_time
-                else:
-                    cross_section_acc_time = 0
-                    cross_section_crash_acc_time = 0
-
-                # final row for full file, and almost final for summary file
-                row = [start_dt.ctime(), end_dt.ctime(), machine, bench, header_info,
-                       (i - first_i + 1), sdc_s, acc_time_s, abort_zero_s, flux,
-                       flux_acc_time, fluency, fluency_acc_time,
-                       cross_section, cross_section_crash, time_beam_off, cross_section_acc_time,
-                       cross_section_crash_acc_time, time_beam_off_acc_time, MAGIC_THERMAL_VALUE]
-
-                writer_csv_summary.writerow(row)
-                i += 1
-
-
-
-"""
+    
 def main():
     if len(sys.argv) < 4:
         print "Usage: %s <neutron counts input file> <csv file> <factor>" % (sys.argv[0])
@@ -288,7 +147,7 @@ def main():
                     "Cross Section Crash", "Time Beam Off (sec)", "Cross Section SDC AccTime",
                     "Cross Section Crash AccTime", "Time Beam Off AccTime (sec)"]
         writer_csv_full.writerow(header_c)
-        # We need to read the neutron count files before calling get_fluency_flux
+        # We need to read the neutron count files before calling get_fluence_flux
         file_lines = read_count_file(in_file_name)
     
         i = -1
@@ -375,7 +234,7 @@ def main():
             # writer_csv_full.writerow([])
             writer_csv_summary.writerow([])
             # writer_csv_summary.writerow([])
-"""
+
 
 #########################################################
 #                    Main Thread                        #
